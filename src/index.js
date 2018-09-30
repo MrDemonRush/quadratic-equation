6f9dc0beaef7decf116f0da834249d238c53d196
MDR <mrdemonrush@gmail.com>
2018-09-30 16:19:21 +0300

    quad-equation 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var arr = equation.split(" ");
+  var a = parseInt(arr[0]);
+  var b = parseInt(arr[3] + arr[4]);
+  var c = parseInt(arr[7] + arr[8]);
+
+  var result = [];
+
+  var b2 = b / 2;
+  var sqD1 = Math.sqrt(b2 * b2 - a * c);
+  result[0] = Math.round((-b2 + sqD1) / a);
+  result[1] = Math.round((-b2 - sqD1) / a);
+  
+  result.sort((a,b) => (a - b));
+  return result;
 }
